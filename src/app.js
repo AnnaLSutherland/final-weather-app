4274150b7f3b8293fc7ee77da0b3f227466cc510
AnnaLSutherland <116560657+AnnaLSutherland@users.noreply.github.com>
2022-12-07 17:35:57 +0000

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursay",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${minutes}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   document.querySelector("#current-temperature").innerHTML =
@@ -15,6 +38,10 @@ function displayTemperature(response) {
     response.data.temperature.humidity;
   document.querySelector("#feels-like").innerHTML =
     Math.round(response.data.temperature.feels_like);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(
+    response.data.time * 1000
+  );
 }
 
 let key = `d36aa0424f0b7a4te0a504eba4fo6786`;

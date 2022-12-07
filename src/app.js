2e7df9ba0bfd3c70be5171bc3ccd57bb0db52729
AnnaLSutherland <116560657+AnnaLSutherland@users.noreply.github.com>
2022-12-07 17:12:08 +0000

    Showing the Current Temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  document.querySelector("#current-temperature").innerHTML =
+    Math.round(response.data.temperature.current);
+  document.querySelector(
+    "#city"
+  ).innerHTML = `${response.data.city},`;
+  document.querySelector("#country").innerHTML =
+    response.data.country;
+  document.querySelector("#description").innerHTML =
+    response.data.condition.description;
+  document.querySelector(`#wind-speed`).innerHTML =
+    Math.round(response.data.wind.speed);
+  document.querySelector(`#humidity`).innerHTML =
+    response.data.temperature.humidity;
+  document.querySelector("#feels-like").innerHTML =
+    Math.round(response.data.temperature.feels_like);
+}
 
+let key = `d36aa0424f0b7a4te0a504eba4fo6786`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Aberdeen&key=${key}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
